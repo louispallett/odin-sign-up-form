dfc327371fabf37422b4378875ef2d4203e07264
louispallett <louis.pallett@outlook.com>
2023-05-11 17:22:12 +0100

    Begun work on themes and permission mismatch error in JS

--- script.js
+++ script.js
@@ -10,3 +10,25 @@ function setTheme()
 }
 
 document.querySelector('.theme-toggle').addEventListener('click', setTheme);
+
+//Password mismatch
+const passwordEle = document.getElementById("password");
+const password = passwordEle.value;
+const confpasswordEle = document.getElementById("confpassword");
+const confpassword = confpasswordEle.value;
+const passwordError = document.getElementById("passwordError");
+
+
+// if (password != confpassword)
+// {
+//     console.log("No match!");  
+// }
+
+console.log(password);
+console.log(confpassword);
+
+while (password != confpassword)
+{
+    passwordError.classList.add("password-mismatch");
+}
+
